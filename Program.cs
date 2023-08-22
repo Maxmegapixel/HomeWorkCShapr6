5e285fa4bd09d11bb6662dd25d9d6af3d9ead18f
Maksim <maxmegapixel@mail.ru>
2023-08-22 19:49:54 +0300

    zadacha 2

--- Program.cs
+++ Program.cs
@@ -4,9 +4,6 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 4
 
-// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 // Console.WriteLine("Введите последовательность чисел через пробел: ");
 // string StVal = Console.ReadLine()!;
@@ -41,21 +38,42 @@
 //     return Num;
 // }
 
-Console.WriteLine("Введите последовательность чисел через пробел: ");
-string StVal = Console.ReadLine()!;
-Console.WriteLine($"Чисел больше нуля: {GetNumber(StVal)}");
+// Console.WriteLine("Введите последовательность чисел через пробел: ");
+// string StVal = Console.ReadLine()!;
+// Console.WriteLine($"Чисел больше нуля: {GetNumber(StVal)}");
 
-int GetNumber(string text)
+// int GetNumber(string text)
+// {
+//     string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+//     int[] Arr = new int[words.Length];
+//     int Count = 0;
+//     for (int i = 0; i < words.Length; i++)
+//     {
+//         Arr[i] = Convert.ToInt32(words[i]);
+//         Count += Arr[i]>0? 1:0;
+//     }
+//     return Count;
+// }
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+PointInt(b1,k1,b2,k2);
+
+void PointInt(double b1, double k1, double b2, double k2)
 {
-    string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-    int[] Arr = new int[words.Length];
-    int Count = 0;
-    for (int i = 0; i < words.Length; i++)
-    {
-        Arr[i] = Convert.ToInt32(words[i]);
-        Count += Arr[i]>0? 1:0;
-    }
-    return Count;
+    double x = ( b2 - b1) /(k1  - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения: ({x};{y})");
 }
-
-
